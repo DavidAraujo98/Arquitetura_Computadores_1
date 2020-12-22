3aed6693772e1e669080d65c6903b98c8063c9d1
David José Araújo Ferreira <david2araujo5@gmail.com>
2020-12-22 18:22:22 +0000

    Guiao 05

--- G5/ex1a.asm
+++ G5/ex1a.asm
@@ -4,33 +4,36 @@
 # lista + i : $t2
 
 	.data
-	.eqv SIZE 5 		
+
+	.eqv SIZE 5
+	.eqv read_int, 5
+	.eqv print_string, 4		
+
 str:	.asciiz	"\nIntroduza um número: "
 	.align 2
-list:	.space 20		
-	.eqv read_int, 5
-	.eqv print_string, 4
+
+list:	.space 20			
 	
 	.text
 	.globl main
 	
-main:	li $t0, 0		
-while:	bge $t0, SIZE, endW	
+main:	li	$t0, 0		
+while:	bge	$t0, SIZE, endW	
 	
-	la $a0, str 				
-	li $v0, print_string
+	la	$a0, str 				
+	li	$v0, print_string
 	syscall
 			
-	li $v0, read_int	
+	li	$v0, read_int	
 	syscall
 	
-	la $t1, list		
-	sll $t2, $t0, 2
+	la	$t1, list		
+	sll	$t2, $t0, 2
 	
-	addu $t2, $t2, $t1	
-	sw $v0, 0($t2)		
-														
-	addi $t0, $t0, 1	
-	j while
-endW: 				
-	jr $ra
+	addu	$t2, $t2, $t1	
+	sw	$v0, 0($t2)		
+												
+	addi	$t0, $t0, 1	
+	j	while
+
+endW: 	jr	$ra
--- G5/ex3.asm
+++ G5/ex3.asm
@@ -16,16 +16,14 @@
 # *p: $t4
 # lista+Size: $t5
 
-
-
 	.data
+
 	.eqv SIZE, 10	#define SIZE 10
 	.eqv TRUE, 1	#define TRUE 1
 	.eqv FALSE,0	#define FALSE 0		
 	.eqv print_int10, 1
 	.eqv read_int, 5
 	.eqv print_string, 4
-
 	
 strA:	.asciiz	"\nIntroduza um número: "
 	.align 2
@@ -39,80 +37,73 @@ list:	.space 40
 	.text
 	.globl main 	
 	
-main:				
-				
-				
-	li  $t0, 0		
-while1:	beq $t0, SIZE, endW1	
+main:	li	$t0, 0		
+while1:	beq	$t0, SIZE, endW1	
 	
-	la $a0, strA 					
-	li $v0, print_string
+	la	$a0, strA 					
+	li	$v0, print_string
 	syscall
 			
-	li $v0, read_int	
+	li	$v0, read_int	
 	syscall
 	
-	la  $t1, list		
-	sll $t2, $t0, 2
+	la	$t1, list		
+	sll	$t2, $t0, 2
 	
-	addu $t2, $t2, $t1	
-	sw $v0, 0($t2)				
-														
-	addi $t0, $t0, 1	
-	j while1
-endW1: 				
-	la $t6, list		
-	
-do:				
-	li $t3, 0		
-	li $t4, FALSE		
+	addu	$t2, $t2, $t1	
+	sw	$v0, 0($t2)				
+												
+	addi	$t0, $t0, 1	
+	j	while1
 
-				
-while:	bge $t3, SIZE, endW	
-
-	sll  $t7, $t3, 2	
-	addu $t7, $t7, $t6	
-	lw $t8,0($t7) 		
-	lw $t9,4($t7) 		
+endW1: 	la	$t6, list		
 	
-if:	ble $t8, $t9, endIf 	
+do:	li	$t3, 0		
+	li	$t4, FALSE		
 				
-	sw $t9, 0($t7)		
-	sw $t8, 4($t7)		
-	li $t4, TRUE		
-endIf: 				
+while:	bge	$t3, SIZE, endW	
+
+	sll	$t7, $t3, 2	
+	addu	$t7, $t7, $t6	
+	
+	lw	$t8,0($t7) 		
+	lw	$t9,4($t7) 		
+	
+if:	ble	$t8, $t9, endIf 	
+				
+	sw	$t9, 0($t7)		
+	sw	$t8, 4($t7)		
+	li	$t4, TRUE
 		
-	addi $t3, $t3, 1	
-	j while		
-endW:				
+endIf: 	addi	$t3, $t3, 1	
+	j	while	
+	
+endW:								
+endDo:	beq	$t4, TRUE, do  	
 				
-endDo:	beq $t4, TRUE, do  	
-
-				
-	li $v0, print_string	
-	la $a0, strB
+	li	$v0, print_string	
+	la	$a0, strB
 	syscall
 	
-	la $t0, list		
+	la	$t0, list		
+							
+	li	$t2, SIZE			
+	sll	$t2, $t2, 2		
+	addu	$t2, $t2, $t0	
 				
-				
-	li $t2, SIZE			
-	sll $t2, $t2, 2		
-	addu $t2, $t2, $t0	
-				
-whileB: bge $t0, $t2, endWB	
-	lw $t1, 0($t0)		
+whileB:	bge	$t0, $t2, endWB	
+	lw	$t1, 0($t0)		
 	
-	li $v0, print_int10	
-	move $a0, $t1
+	li	$v0, print_int10	
+	move	$a0, $t1
 	syscall
 					
-	li $v0, print_string	
-	la $a0, strC
+	li	$v0, print_string	
+	la	$a0, strC
 	syscall			
 
-	addu $t0, $t0, 4	
+	addu	$t0, $t0, 4	
 
-	j whileB			
-endWB:		
-	jr $ra			
+	j	whileB			
+
+endWB:	jr	$ra			
